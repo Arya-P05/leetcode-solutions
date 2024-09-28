ff5aaec2e8835ebcd6ffabd10d84234ad75c2f3f
Arya <jackrack040@gmail.com>
2024-09-28 12:12:10 -0400

    238. Product of Array Except Self

---
+++ 238. Product of Array Except Self.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        output = [1] * len(nums)
+
+        prefix = 1
+
+        for idx in range(len(nums)):
+            output[idx] = prefix
+            prefix *= nums[idx]
+        
+        suffix = 1
+
+        for idx in range(len(nums) - 1, -1, -1):
+            output[idx] *= suffix
+            suffix *= nums[idx]
+        
+        return output
+    
+def main():
+    nums = input("Enter a list of integers separated by spaces: ").strip().split()
+
+    nums = [int(num) for num in nums]
+
+    solution = Solution()
+    result = solution.productExceptSelf(nums)
+
+    print("The product except self array is:", result)
+
+if __name__ == "__main__":
+    main()
