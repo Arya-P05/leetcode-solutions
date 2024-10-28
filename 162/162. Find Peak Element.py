14be780e91d125477d46fa5dea71a0c04292faec
Arya <jackrack040@gmail.com>
2024-10-28 14:22:38 -0400

    162

--- 162/162. Find Peak Element.py
+++ 162/162. Find Peak Element.py
@@ -3,22 +3,16 @@ from typing import List
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         length = len(nums)
+        left = 0
+        right = length - 1
 
-        if length == 1:
-            return 0
-        elif length == 2:
-            if nums[1] > nums[0]:
-                return 1
+        while (left < right):
+            mid = (right + left) // 2
+
+            if (nums[mid] > nums[mid+1]):
+                right = mid
             else:
-                return 0
-        else:
-            for idx in range(length):
-                if idx == 0:
-                    if nums[idx] > nums[idx+1]:
-                        return idx
-                elif idx == length - 1:
-                    if nums[idx-1] < nums[idx]:
-                        return idx
-                elif nums[idx-1] < nums[idx] and nums[idx] > nums[idx+1]:
-                    return idx
+                left = mid + 1
+
+        return left
             
