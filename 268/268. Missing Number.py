6a4e1aa5415377a5a210bac37c4a268b7d2f2927
Arya <jackrack040@gmail.com>
2024-11-01 15:12:05 -0400

    268

--- 268/268. Missing Number.py
+++ 268/268. Missing Number.py
@@ -2,6 +2,17 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # This solution above is O(n) time complexity and O(1) space complexity
+
+        goal = range(len(nums) + 1)
+
+        missing_num = sum(goal) - sum(nums)
+
+        return missing_num
+
+        '''
+        First solution (Time: O(n), Space: O(n)):
+
         new = set()
 
         for number in range(len(nums)+1):
@@ -12,3 +23,4 @@ class Solution:
         
         for result in new:
             return result
+        '''
