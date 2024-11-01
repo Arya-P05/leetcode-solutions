c122197b1fbef5b5c9a916c886fa585b22992904
Arya <jackrack040@gmail.com>
2024-11-01 15:21:15 -0400

    151

--- 151/151. Reverse Words in a String.py
+++ 151/151. Reverse Words in a String.py
@@ -15,12 +15,3 @@ class Solution:
             tempWord = ""
         
         return " ".join(output[::-1])
-    
-def main():
-    s = str(input("What sentence do you want to reverse? "))
-    sol = Solution()
-    sol = sol.reverseWords(s)
-    print(f"The reversed string is: {sol}")
-
-if __name__ == "__main__":
-    main()
