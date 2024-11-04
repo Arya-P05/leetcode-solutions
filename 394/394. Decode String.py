f8275136a6ea1e2570fd1a43cc7ca464e2797908
Arya <jackrack040@gmail.com>
2024-11-03 21:31:13 -0500

    394

--- 394/394. Decode String.py
+++ 394/394. Decode String.py
@@ -4,3 +4,23 @@ class Solution:
         # then you add to a temp string until you get to "["
         # then you get the amount of times it repeats by checking if its a digit
         # add the temp string times the number you get to the stack end
+
+        stack = []
+
+        for idx in range(len(s)):
+            if s[idx] != ']':
+                stack.append(s[idx])
+            else:
+                code = ""
+                while(stack and stack[-1] != '['):
+                    code = stack.pop() + code
+                stack.pop()
+
+                repeat = ""
+                while(stack and stack[-1].isdigit()):
+                    repeat = stack.pop() + repeat
+                
+                stack.append(int(repeat) * code)
+        
+        return "".join(stack)
+    
