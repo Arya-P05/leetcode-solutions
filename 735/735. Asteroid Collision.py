b8743ae675788b21c55fd1e0c1e5efe5a8e3bd50
Arya <jackrack040@gmail.com>
2024-11-04 00:21:05 -0500

    735

--- 735/735. Asteroid Collision.py
+++ 735/735. Asteroid Collision.py
@@ -2,38 +2,18 @@ from typing import List
 
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        def do(asteroids):
-            stack = []
-            idx = 0
+        stack = []
 
-            while (idx < (len(asteroids) - 1)):
-                if asteroids[idx] * asteroids[idx + 1] >= 0:
-                    stack.append(asteroids[idx])
-                elif abs(asteroids[idx]) > abs(asteroids[idx + 1]):
-                    stack.append(asteroids[idx])
-                    idx += 1
-                elif abs(asteroids[idx]) < abs(asteroids[idx + 1]):
-                    stack.append(asteroids[idx + 1])
-                    idx += 1
-                else:
-                    idx += 1
-
-                idx += 1
-            
-            return stack
-        
-        done = False
-        maybe = do(asteroids)
-        while(not done):
-            for idx in range(len(maybe)-1):
-                if idx == len(maybe)-2 and maybe[idx] * maybe[idx+1] > 0:
-                    done = True
-                elif maybe[idx] * maybe[idx+1] < 0:
-                    maybe = do(maybe)
+        for asteroid in asteroids:
+            while (stack and (asteroid < 0 < stack[-1])):
+                if stack[-1] < -asteroid:
+                    stack.pop()
+                elif stack[-1] == -asteroid:
+                    stack.pop()
                     break
+                else:
+                    break
+            else:
+                stack.append(asteroid)
         
-        return maybe
-            
-            
-                
-
+        return stack
