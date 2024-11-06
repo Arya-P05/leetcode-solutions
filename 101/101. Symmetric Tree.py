cbd90015d6f2c23cdc5bd73e5983a172ebde8505
Arya <jackrack040@gmail.com>
2024-11-06 15:18:50 -0500

    101

---
+++ 101/101. Symmetric Tree.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def areBothSymmetric(left, right):
+            if (left is None and right is None):
+                return True
+            elif (left and right is None) or (left is None and right):
+                return False
+            elif (left.val != right.val):
+                return False
+            else:
+                return areBothSymmetric(left.left, right.right) and areBothSymmetric(left.right, right.left)
+        
+        if (root is None):
+            return True
+        else:
+            return areBothSymmetric(root.left, root.right)
