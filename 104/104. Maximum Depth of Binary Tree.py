de5bd17d54433d7fe0391f1c9fb762cb72d24f65
Arya <jackrack040@gmail.com>
2024-11-11 09:54:06 -0500

    104

--- 104/104. Maximum Depth of Binary Tree.py
+++ 104/104. Maximum Depth of Binary Tree.py
@@ -12,5 +12,8 @@ class Solution:
         if root is None:
             return 0
         else:
-            return max(1 + self.maxDepth(root.left), 1 + self.maxDepth(root.right))
+            left_depth = self.maxDepth(root.left)
+            right_depth = self.maxDepth(root.right)
+            
+            return max(left_depth, right_depth) + 1
         
