ad4975a8938d7867aa2612512f2bc3de59a4c8b7
Arya <jackrack040@gmail.com>
2024-12-02 11:15:35 -0500

    1372

---
+++ 1372/1372. Longest ZigZag Path in a Binary Tree.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        self.len_longest_zigzag = 0
+
+        def dfs(node, left, right):
+            self.len_longest_zigzag = max(self.len_longest_zigzag, left, right)
+
+            if node.left:
+                dfs(node.left, right + 1, 0)
+
+            if node.right:
+                dfs(node.right, 0, left + 1)
+
+        dfs(root, 0, 0)
+        return self.len_longest_zigzag
