da1c3b13b5091a5e4537d7bf976dbc0c38a7e6fe
Arya <jackrack040@gmail.com>
2025-05-21 18:37:23 -0400

    36

--- 36/36. Valid Sudoku.py
+++ 36/36. Valid Sudoku.py
@@ -3,24 +3,26 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
+        curr_row = set()
         cols = defaultdict(set)
-        squares = defaultdict(set)
+        sub_boxes = defaultdict(set)
 
-        for row in range(9):
-            for col in range(9):
-                digit = board[row][col]
+        for row_idx in range(9):
+            for col_idx in range(9):
+                curr_cell = board[row_idx][col_idx]
 
-                if digit == ".":
+                if curr_cell == '.':
                     continue
-                
-                if digit in rows[row] or digit in cols[col] or digit in squares[row // 3, col // 3]:
+
+                if curr_cell in curr_row or curr_cell in sub_boxes[(row_idx//3, col_idx//3)] or curr_cell in cols[col_idx]:
                     return False
                 else:
-                    rows[row].add(digit)
-                    cols[col].add(digit)
-                    squares[row // 3, col // 3].add(digit)
+                    curr_row.add(curr_cell)
+                    sub_boxes[(row_idx//3, col_idx//3)].add(curr_cell)
+                    cols[col_idx].add(curr_cell)
 
+            curr_row = set()
+        
         return True
                    
         
