39e7a1cd0aaab556aba92ac4ff1b50e8e5cb0370
Arya <jackrack040@gmail.com>
2025-05-26 11:07:16 -0400

    128

--- 128/128. Longest Consecutive Sequence.py
+++ 128/128. Longest Consecutive Sequence.py
@@ -2,21 +2,19 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numbers = sorted(set(nums)) # is O(nlog(n)) because of sorted
-        longest = [1] * len(nums)
-        index = 0
+        numbers = set(nums)
+        longest = 0
 
-        for idx in range(len(numbers)-1):
-            if numbers[idx] + 1 == numbers[idx+1]:
-                longest[index] += 1
-            else:
-                index += 1
-        
-        biggest = 0
+        for number in numbers:
+            if number - 1 not in numbers:
+                curr = number
+                streak = 1
 
-        for number in longest:
-            if number > biggest:
-                biggest = number
+                while curr + 1 in numbers:
+                    streak += 1
+                    curr += 1
+                
+                longest = max(streak, longest)
         
-        return biggest
+        return longest
             
